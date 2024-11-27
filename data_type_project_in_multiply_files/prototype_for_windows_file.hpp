a9cfc83b67dfa558e8907b205c1a946bdfff76f3
big smoke <rydern637@gmail.com>
2024-11-27 17:38:03 +0330

    Update prototype_for_windows_file.hpp

--- data_type_project_in_multiply_files/prototype_for_windows_file.hpp
+++ data_type_project_in_multiply_files/prototype_for_windows_file.hpp
@@ -5,6 +5,8 @@
 
 void change_xy(int, int);
 int terminals_lenght_in_run_time();
+int terminals_width_in_run_time();
 void saved_terminal_content ( std::vector<std::string>&);
+void fill_the_buffer(char char_to_fill, int x_coordinate, int y_coordinate, int char_number_to_fill_in_each_line);
 
 #endif 
